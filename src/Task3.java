dfb2c30969a67dad11d0ab56d6acee25d0e1c725
Mayur <mayurharisinghani@gmail.com>
2025-03-03 11:10:21 -0700

    Task 3 Complete

---
+++ src/Task3.java
@@ -0,0 +1,8 @@
+public class Task3 {
+    public static void main(String[] args) {
+        for (int c = 0; c < 19; c+= 3){
+            System.out.printf("%3d |", c);
+        }
+        System.out.println("\n");
+    }
+}
