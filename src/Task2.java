9c4817e53d3a9e948ffed6010590a43d3fb1bf8e
Mayur <mayurharisinghani@gmail.com>
2025-03-03 11:07:47 -0700

    Task 2 Complete

---
+++ src/Task2.java
@@ -0,0 +1,8 @@
+public class Task2 {
+    public static void main(String[] args) {
+        for (int c = 30; c > -1; c--){
+            System.out.printf("%3d |", c);
+        }
+        System.out.println("\n");
+    }
+}
