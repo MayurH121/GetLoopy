f30818488962bec2434b6209ea3fa1e5384f244c
Mayur <mayurharisinghani@gmail.com>
2025-03-03 12:10:24 -0700

    Task 6 Complete

---
+++ src/Task6.java
@@ -0,0 +1,10 @@
+public class Task6 {
+    public static void main(String[] args) {
+        for (int l = 5; l >= 1; l--) {
+            for (int w = 1; w <= l; w++) {
+                System.out.print("* "); // Print a star followed by a space
+            }
+            System.out.println();
+        }
+    }
+}
